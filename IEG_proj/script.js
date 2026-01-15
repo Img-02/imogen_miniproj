4ccff083761a05b6ba69860331e15df1709101bc
Img-02 <imogengreig02@gmail.com>
2026-01-15 20:11:29 +0000

    Did a bunch of stuff

--- IEG_proj/script.js
+++ IEG_proj/script.js
@@ -1,26 +1,50 @@
-//messing around with on click and alert event 
-let cell = document.getElementsByClassName("cell");
+let board = ["","","","","","","","","",];
+let currentPlayer = "O"
+let gameActive = true;
+let score = 0;
 
-if (cell.length > 0){
-cell[0].addEventListener("click", () => {
-    console.log("testing");
-    alert('div clicked (classbased)')
-    
-});
-}
 //for n&c game need
-// *start game function 
-// *select panel function (on click event?)
+//*start game function 
 //*select user type function (x or o)
 //*check for winning combos
 
-let player1 = "X";
-let player2 = "O"
-let gameOver = false;
-let score = 0;
+//had to look these up lol
+const winConditions = [
+       [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
 
-//put winning combos here 
 
-function selectPanel(){
+//all div cells are selected 
+const cells = document.querySelectorAll(".cell");
+
+//.innerText sets HTML content of element 
+function place(play) {
+    //if (gameActive == true)
+      play.innerText = currentPlayer; //add text for each play 
+      if (currentPlayer == "O"){ //let O player place 0
+        currentPlayer = "X"; //change O to X
+        alert("Pass the computer to Player 2");
+      }
+      else {
+        currentPlayer = "O"; //continues with O player which is default 
+        alert("Pass the computer to Player 1");
+        }
+    }
+
+function checkWinner(){
+
+}
+
+function restartGame(){
+    //button to wipe board set all to blank
     
 }
+
+function startGame(){
+    //add a button to start game
+    //check if gameActive set to true, change if not
+    //load board ? 
+}
+
