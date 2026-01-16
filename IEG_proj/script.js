c4a84a6b30df40e7cade1af7b3d5615c100423ab
Img-02 <imogengreig02@gmail.com>
2026-01-16 11:34:34 +0000

    change something

--- IEG_proj/script.js
+++ IEG_proj/script.js
@@ -1,3 +1,4 @@
+
 let board = ["","","","","","","","","",];
 let currentPlayer = "O"
 let gameActive = true;
@@ -11,8 +12,8 @@ let score = 0;
 //had to look these up lol
 const winConditions = [
        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6] // Diagonals
+       [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+       [0, 4, 8], [2, 4, 6] // Diagonals
     ];
 
 
@@ -23,7 +24,7 @@ const cells = document.querySelectorAll(".cell");
 function place(play) {
     //if (gameActive == true)
       play.innerText = currentPlayer; //add text for each play 
-      if (currentPlayer == "O"){ //let O player place 0
+      if (currentPlayer == "O"){ 
         currentPlayer = "X"; //change O to X
         alert("Pass the computer to Player 2");
       }
